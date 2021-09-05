d2b3c94bca4669c1d56a27e10118153f375dfd57
calpa <calpaliu@gmail.com>
2021-09-05 10:09:30 -0700

    Init obsidian-font-awesome-plugin

--- main.ts
+++ main.ts
@@ -1,112 +1,59 @@
-import { App, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Plugin, PluginSettingTab, Setting } from "obsidian";
 
-interface MyPluginSettings {
-	mySetting: string;
+interface PluginSettings {
+  kitName: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: PluginSettings = {
+  kitName: "default",
+};
+
+export default class FontAwesomePlugin extends Plugin {
+  settings: PluginSettings;
+
+  async onload() {
+    await this.loadSettings();
+
+    const script = document.createElement("script");
+    script.src = `https://kit.fontawesome.com/${this.settings.kitName}.js`;
+    script.crossOrigin = "anonymous";
+    document.head.appendChild(script);
+
+    this.addSettingTab(new FontAwesomeSettingTab(this.app, this));
+  }
+
+  async loadSettings() {
+    this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+  }
+
+  async saveSettings() {
+    await this.saveData(this.settings);
+  }
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+class FontAwesomeSettingTab extends PluginSettingTab {
+  plugin: FontAwesomePlugin;
 
-	async onload() {
-		console.log('loading plugin');
+  constructor(app: App, plugin: FontAwesomePlugin) {
+    super(app, plugin);
+    this.plugin = plugin;
+  }
 
-		await this.loadSettings();
+  display(): void {
+    let { containerEl } = this;
 
-		this.addRibbonIcon('dice', 'Sample Plugin', () => {
-			new Notice('This is a notice!');
-		});
+    containerEl.empty();
 
-		this.addStatusBarItem().setText('Status Bar Text');
+    containerEl.createEl("h2", { text: "Font Awesome" });
 
-		this.addCommand({
-			id: 'open-sample-modal',
-			name: 'Open Sample Modal',
-			// callback: () => {
-			// 	console.log('Simple Callback');
-			// },
-			checkCallback: (checking: boolean) => {
-				let leaf = this.app.workspace.activeLeaf;
-				if (leaf) {
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-					return true;
-				}
-				return false;
-			}
-		});
-
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		this.registerCodeMirror((cm: CodeMirror.Editor) => {
-			console.log('codemirror', cm);
-		});
-
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-		console.log('unloading plugin');
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		let {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		let {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		let {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue('')
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+    new Setting(containerEl).setName("Kit Name").addText((text) =>
+      text
+        .setPlaceholder("Enter your kit name")
+        .setValue("")
+        .onChange(async (value) => {
+          this.plugin.settings.kitName = value;
+          await this.plugin.saveSettings();
+        })
+    );
+  }
 }
